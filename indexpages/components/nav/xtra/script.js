3d6f2d34e8ef269a4528c03bbee3613d12d869e7
Madeleine <mgleahy@nic.edu>
2023-05-08 12:39:17 -0700

    Base Code

---
+++ indexpages/components/nav/xtra/script.js
@@ -0,0 +1,4 @@
+// const toggleButton = document.getElementsByClassName ('toggle-button') [0]
+// const navbarLinks = document.getElementsByClassName ('navbar-links') [0]
+
+// toggleButton.addEventListener ('click', () => { navbarLinks.classlist.toggle ('active') } )
